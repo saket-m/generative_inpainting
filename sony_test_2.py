012ed160fc4c800392f97ab0f8c9bcbd1c0141a9
Ubuntu <ubuntu@ip-172-31-32-254.ec2.internal>
2020-05-11 10:53:12 +0000

    added sony_test_2.py

---
+++ sony_test_2.py
@@ -0,0 +1,117 @@
+import argparse
+
+import cv2
+import numpy as np
+import tensorflow as tf
+import neuralgym as ng
+import os
+import shutil
+
+
+from inpaint_model import InpaintCAModel
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--checkpoint_dir', default='', type=str,
+                    help='The directory of tensorflow checkpoint.')
+
+
+if __name__ == "__main__":
+    FLAGS = ng.Config('inpaint.yml')
+    # ng.get_gpus(1)
+    args, unknown = parser.parse_known_args()
+    checkpoint_dir = args.checkpoint_dir
+
+    '''
+    this script takes a video and removes logo from it, the logo
+    should be present in a fixed  location across all the frames.
+    '''
+    ## 1. set the input video dir, it may contain multiple videos and 
+    ## the frame position map
+    VIDEO_DIR = '/home/saket/vision/sony/generative_inpainting/examples/sony/input_videos'
+    MASK_DIR = '/home/saket/vision/sony/generative_inpainting/examples/sony/mask'
+    OUTPUT_DIR = '/home/saket/vision/sony/generative_inpainting/examples/sony/output_videos'
+    TEMP_DIR = '/home/saket/vision/sony/generative_inpainting/examples/sony/temp'
+    TEMP_INPUT_DIR = '/home/saket/vision/sony/generative_inpainting/examples/sony/temp/input'
+    TEMP_OUTPUT_DIR = '/home/saket/vision/sony/generative_inpainting/examples/sony/temp/output'
+
+    if not os.path.exists(VIDEO_DIR):
+        print('E :: inout video dir does not exist.')
+        exit()
+    if not os.path.isdir(MASK_DIR):
+        print('E :: inout mask dir does not exist.')
+        exit()
+    if not os.path.isdir(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+    if not os.path.isdir(TEMP_DIR):
+        os.mkdir(TEMP_DIR)
+    if not os.path.isdir(TEMP_INPUT_DIR):
+        os.mkdir(TEMP_INPUT_DIR)
+    if not os.path.isdir(TEMP_OUTPUT_DIR):
+        os.mkdir(TEMP_OUTPUT_DIR)
+
+    ## 2. load the tensorflow model and create a session
+    ## load pretrained model
+    model = InpaintCAModel()
+    sess_config = tf.ConfigProto()
+    sess_config.gpu_options.allow_growth = True
+    sess = tf.Session(config=sess_config)
+    input_image = tf.placeholder(shape = (1, 480, 1312, 3), dtype=tf.float32, name='fdfd')
+    output = model.build_server_graph(FLAGS, input_image)
+    output = (output + 1.) * 127.5
+    output = tf.reverse(output, [-1])
+    output = tf.saturate_cast(output, tf.uint8)
+    vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    assign_ops = []
+    for var in vars_list:
+        vname = var.name
+        from_name = vname
+        var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
+        assign_ops.append(tf.assign(var, var_value))
+    sess.run(assign_ops)
+    print('Model loaded.')
+    
+
+    ## 3. iterate over the videos and extract frames and based on the position
+    ## map crear the logo and re paint the frame
+    print(1, os.listdir(VIDEO_DIR))
+    for video_name in os.listdir(VIDEO_DIR):
+        video_path = os.path.join(VIDEO_DIR, video_name)
+        mask_path = os.path.join(MASK_DIR, video_name).replace('mp4', 'png')
+        print(2, mask_path, video_path)
+
+        shutil.rmtree(TEMP_INPUT_DIR)
+        shutil.rmtree(TEMP_OUTPUT_DIR)
+        os.mkdir(TEMP_INPUT_DIR)
+        os.mkdir(TEMP_OUTPUT_DIR)
+        os.system(f'ffmpeg -i {video_path} {TEMP_INPUT_DIR}/%05d.png')
+
+        for image_name in os.listdir(TEMP_INPUT_DIR):
+            image_path = os.path.join(TEMP_INPUT_DIR, image_name)
+            output_path = os.path.join(TEMP_OUTPUT_DIR, image_name)
+
+            ## 4. load the image and mask for inference
+            image = cv2.imread(image_path)
+            mask = cv2.imread(mask_path)
+            # mask = cv2.resize(mask, (0,0), fx=0.5, fy=0.5)
+
+            assert image.shape == mask.shape
+
+            h, w, _ = image.shape
+            grid = 8
+            image = image[:h//grid*grid, :w//grid*grid, :]
+            mask = mask[:h//grid*grid, :w//grid*grid, :]
+            print('Shape of image: {}'.format(image.shape))
+
+            image = np.expand_dims(image, 0)
+            mask = np.expand_dims(mask, 0)
+            input_image_mask = np.concatenate([image, mask], axis=2)           
+            
+            result = sess.run(output, feed_dict={input_image:input_image_mask})
+            cv2.imwrite(output_path, result[0][:, :, ::-1])
+
+            # test(image_path, mask_path, checkpoint_dir, output_path)
+            
+        ## 4. stitch the repainted frames and store them in a output dir
+        os.system(f'ffmpeg -i {TEMP_OUTPUT_DIR}/%05d.png {OUTPUT_DIR}/{video_name}')
+    
